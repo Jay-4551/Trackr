9a7057ad54c865f5ad99ac7c7d71b3ca845fb38b
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-03-20 20:57:09 -0400

    Hero Section Additions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,30 @@
+<script>
+    import {Library, Wallet} from 'lucide-svelte';
+</script>
 <svelte:head>
 	<title>Trackr</title>
 </svelte:head>
 <main>
-    <div class="flex md:flex-row flex-col h-screen w-full items-center justify-evenly">
-        <div class="flex flex-col">
-            <h1 class="text-8xl">Trackr</h1>
-            <p class="text-3xl my-8 text-slate-500">A Website For Game Collectors.</p>
-            <button class="text-3xl text-white bg-gray-800 px-6 py-2 rounded-sm w-fit">Join Now!</button>
+    <div class="flex flex-col h-screen w-full items-center justify-center">
+        <div class=" flex md:flex-row flex-col justify-evenly w-full">
+            <div class="flex flex-col">
+                <h1 class="text-8xl">Trackr</h1>
+                <p class="text-3xl my-8 text-slate-600">A Website For Game Collectors.</p>
+                <button class="text-3xl text-white bg-gray-800 px-6 py-2 rounded-sm w-fit">Join Now!</button>
+            </div>
+            <div>
+                <img src="assets/Gameboy Hero Asset.png" alt="Gameboy Hero Image">
+            </div>
         </div>
-        <div>
-            <img src="assets/Gameboy Hero Asset.png" alt="Gameboy Hero Image">
+        <div class="flex md:flex-row flex-col w-full justify-evenly">
+            <div class="flex flex-col p-4 border rounded">
+                <Library size={75} class="self-center"/>
+                <p class="text-slate-600 text-xl max-w-[45ch]">Track Your Gaming Collection</p>
+            </div>
+            <div class="flex flex-col p-4 border rounded">
+                <Wallet size={75} class="self-center"/>
+                <p class="text-slate-600 text-xl max-w-[45ch]">Track The Price of Games</p>
+            </div>
         </div>
     </div>
 </main>
