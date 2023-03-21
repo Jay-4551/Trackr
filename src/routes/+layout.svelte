221d4284581031c04ac7768a8b51d0442730a4c3
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-03-20 20:06:48 -0400

    Added Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,12 @@
   </script>
   
   <slot />
-  <nav>
-    <a href="">Home</a>
-    <a href="">Library</a>
-    <a href="">Tracking</a>
-    <button>Sign Up</button>
+  <nav class="flex flex-row justify-around p-4 items-center">
+    <a href="/" class="text-xl font-semibold">Trackr</a>
+    <div class="font-semibold">
+    <a href="/" class="px-4">Home</a>
+    <a href="/library" class="px-4">Library</a>
+    <a href="/tracking" class="px-4">Tracking</a>
+    </div>
+    <button class="rounded-sm py-2 px-4 bg-gray-800 text-white">Sign Up</button>
   </nav>
