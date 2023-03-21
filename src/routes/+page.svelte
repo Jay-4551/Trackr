e9d5eafabdfca78afa7deeb62b6a09e3bfca859f
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-03-20 21:00:08 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 </svelte:head>
 <main>
     <div class="flex flex-col h-screen w-full items-center justify-center">
-        <div class=" flex md:flex-row flex-col justify-evenly w-full">
+        <div class=" flex md:flex-row flex-col justify-evenly w-full items-center">
             <div class="flex flex-col">
                 <h1 class="text-8xl">Trackr</h1>
                 <p class="text-3xl my-8 text-slate-600">A Website For Game Collectors.</p>
@@ -16,10 +16,10 @@
                 <img src="assets/Gameboy Hero Asset.png" alt="Gameboy Hero Image">
             </div>
         </div>
-        <div class="flex md:flex-row flex-col w-full justify-evenly">
+        <div class="flex md:flex-row flex-col w-full justify-evenly mt-16">
             <div class="flex flex-col p-4 border rounded">
                 <Library size={75} class="self-center"/>
-                <p class="text-slate-600 text-xl max-w-[45ch]">Track Your Gaming Collection</p>
+                <p class="text-slate-600 text-xl max-w-[45ch]">Log Your Gaming Collection</p>
             </div>
             <div class="flex flex-col p-4 border rounded">
                 <Wallet size={75} class="self-center"/>
