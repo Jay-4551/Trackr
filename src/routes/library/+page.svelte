b4e36729adbee18694c5fcb4554a663b90e750c8
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-03-24 15:56:39 -0400

    Update +page.svelte

--- src/routes/library/+page.svelte
+++ src/routes/library/+page.svelte
@@ -5,13 +5,21 @@
 	<title>Trackr - Library</title>
 </svelte:head>
 <main>
-    <div class="flex flex-col h-screen w-full items-center justify-center py-6 bg-[#0B090A]" style="">
-        <div class="flex flex-row p-4 items-center w-5/6 justify-between rounded-sm bg-[#161A1D] border border-gray-600">
+    <div class="flex flex-col h-screen w-full items-center py-6 bg-[#0B090A]" style="">
+        <div class="flex flex-row p-4 items-center w-5/6 justify-between rounded-sm bg-[#161A1D] border border-gray-600 font-semibold">
             <h2 class="text-3xl text-white">Your Library</h2>
-            <div class="flex flex-row text-[#E5383B] items-end h-full">
+            <div class="flex flex-row text-[#E5383B] items-end h-full text-lg">
                  <p>Filters</p>
                  <ChevronDown/>
             </div>
+        </div>
+        <div class="flex flex-row h-[50vh] w-5/6 rounded-sm bg-[#161A1D] border border-gray-600 font-semibold my-10">
+            <h2 class="text-3xl text-white self-center m-auto">Add Games To Your Library!</h2> 
+        </div>
+        <div class="flex flex-row w-5/6 justify-end">
+            <button class="rounded-sm py-2 px-4 bg-[#161A1D] hover:bg-[#BA181B] duration-300 border border-red-500 text-white text-3xl font-semibold self-end">
+                Add Game +
+            </button>
         </div>
     </div>
 </main>
