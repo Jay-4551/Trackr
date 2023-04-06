456a9c0e861f77f5aa5c90365e8a3d26b429158c
jcleme <clemensjosh@protonmail.com>
2023-04-06 12:28:25 -0400

    1st attempt at checking # of platforms

--- src/routes/tracking/[searchText]/+page.svelte
+++ src/routes/tracking/[searchText]/+page.svelte
@@ -4,11 +4,24 @@
   export let data: PageData;
 </script>
 
-{#each data.data as game}
-  <h1>{game.name}</h1>
-  <h2>{game.id}</h2>
-  {#each game.platforms as platform}
+{#if data.data.hasOwnProperty("length")}
+  {#each data.data as game}
+    <h1>{game.name}</h1>
+    <h2>{game.id}</h2>
+    <!-- {#if game.platforms.length > 0}
+      {#each game.platforms as platform}
+        <div>{platform.abbreviation}</div>
+        <div>{platform.name}</div>
+      {/each}
+    {:else} -->
+    <!-- TODO add a version for single platform -->
+    <!-- {/if} -->
+  {/each}
+{:else}
+  <h1>{data.data.name}</h1>
+  <h2>{data.data.id}</h2>
+  {#each data.data.platforms as platform}
     <div>{platform.abbreviation}</div>
     <div>{platform.name}</div>
   {/each}
-{/each}
+{/if}
